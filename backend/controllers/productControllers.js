9732a1f95eca2c2c09a50cb74f97cf3896097fcd
Nicole Pardal <nicolepardal@Nicoles-MacBook-Pro.local>
2024-08-05 00:55:22 -0400

    implemnted pagination and searching

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,19 +1,29 @@
 import Product from "../models/product.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import APIFilters from "../utils/apiFilters.js";
 
-// Create new Product   =>  /api/v1/products
+
 export const getProducts = async (req, res) => {
-  const products = await Product.find();
+    const resPerPage = 4;
+    const apiFilters = new APIFilters(Product, req.query).search().filters();
+  
+    let products = await apiFilters.query;
+    let filteredProductsCount = products.length;
+  
+    apiFilters.pagination(resPerPage);
+    products = await apiFilters.query.clone();
+  
+    res.status(200).json({
+      resPerPage,
+      filteredProductsCount,
+      products,
+    });
+  };
 
-  res.status(200).json({
-    products,
-  });
-};
 
 // Create new Product   =>  /api//admin/products
 export const newProduct = async (req, res) => {
   const product = await Product.create(req.body);
-
   res.status(200).json({
     product,
   });
@@ -22,11 +32,9 @@ export const newProduct = async (req, res) => {
 // Get single product details   =>  /api/products/:id
 export const getProductDetails = async (req, res, next) => {
   const product = await Product.findById(req?.params?.id);
-
   if (!product) {
     return next(new ErrorHandler("Product Not Found", 404))
   }
-
   res.status(200).json({
     product,
   });
@@ -38,17 +46,14 @@ export const getProductDetails = async (req, res, next) => {
 export const updateProduct = async (req, res) => {
     try {
         let product = await Product.findById(req.params.id);
-
         if (!product) {
             return res.status(404).json({
                 error: "Product not found",
             });
         }
-
         product = await Product.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
         });
-
         res.status(200).json({
             product,
         });
@@ -60,7 +65,6 @@ export const updateProduct = async (req, res) => {
 // Delete product   =>  /api/products/:id
 export const deleteProduct = async (req, res) => {
   const product = await Product.findById(req?.params?.id);
-
   if (!product) {
     return res.status(404).json({
       error: "Producttt not found",
@@ -68,7 +72,6 @@ export const deleteProduct = async (req, res) => {
   }
 
   await product.deleteOne();
-
   res.status(200).json({
     message: "Product Deleted",
   });
